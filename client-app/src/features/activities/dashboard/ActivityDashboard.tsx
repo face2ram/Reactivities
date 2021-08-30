6622eb2a26ef7382effd3307197f678bc37369e0
face2ram <bgsanap@gmail.com>
2021-08-30 22:12:58 +0530

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,49 +1,28 @@
 import React from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
 import ActivityDetails from '../details/ActivityDetails'
 import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
-interface Props {
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+import { useStore } from './../../../app/stores/store';
+import { observer } from 'mobx-react-lite'
 
-const ActivityDashboard = ({ activities, selectedActivity, deleteActivity,
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting }: Props) => {
+const ActivityDashboard = () => {
+    const { activityStore } = useStore();
+    const { selectedActivity, editMode } = activityStore;
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities}
-                    selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}
-                    submitting={submitting}
-                />
+                <ActivityList />
             </Grid.Column>
             <GridColumn width='6'>
                 {selectedActivity && !editMode &&
-                    <ActivityDetails activity={selectedActivity}
-                        cancelSelectActivity={cancelSelectActivity}
-                        openForm={openForm}
-                    />
+                    <ActivityDetails />
                 }
                 {editMode &&
-                    <ActivityForm
-                        closeForm={closeForm}
-                        activity={selectedActivity}
-                        createOrEdit={createOrEdit}
-                        submitting={submitting} />}
+                    <ActivityForm />}
             </GridColumn>
         </Grid>
     )
 }
 
-export default ActivityDashboard
+export default observer(ActivityDashboard)
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,12 @@
-import React, { ChangeEvent, useState } from 'react'
+import { observer } from 'mobx-react-lite';
+import { ChangeEvent, useState } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
-import { Activity } from './../../../app/models/activity';
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    submitting: boolean;
-}
+import { useStore } from './../../../app/stores/store';
 
-const ActivityForm = ({ activity: selectedActivity, closeForm, createOrEdit, submitting }: Props) => {
+const ActivityForm = () => {
+    const { activityStore } = useStore();
+    const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
+
     const initialState = selectedActivity ?? {
         id: '',
         title: '',
@@ -21,7 +19,7 @@ const ActivityForm = ({ activity: selectedActivity, closeForm, createOrEdit, sub
 
     const [activity, setActivity] = useState(initialState);
     function handleSumbit() {
-        createOrEdit(activity);
+        activity.id ? updateActivity(activity) : createActivity(activity);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
@@ -38,11 +36,11 @@ const ActivityForm = ({ activity: selectedActivity, closeForm, createOrEdit, sub
                 <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
 }
 
-export default ActivityForm
+export default observer(ActivityForm)
