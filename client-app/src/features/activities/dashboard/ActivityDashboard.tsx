9f55d52a941a0f9a672bd35da01c1d9937527b33
face2ram <bgsanap@gmail.com>
2021-09-10 15:06:00 +0530

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
-import ActivityDetails from '../details/ActivityDetails'
-import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
 import { useStore } from './../../../app/stores/store';
 import { observer } from 'mobx-react-lite'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 const ActivityDashboard = () => {
     const { activityStore } = useStore();
-    const { selectedActivity, editMode } = activityStore;
+    const { loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+        if (activityRegistry.size === 0) loadActivities();
+    }, [activityRegistry.size, loadActivities])
+
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
+
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
             </Grid.Column>
             <GridColumn width='6'>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />
-                }
-                {editMode &&
-                    <ActivityForm />}
+                <h2>Activity filters</h2>
             </GridColumn>
         </Grid>
     )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,18 @@
 import { observer } from 'mobx-react-lite';
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState, useEffect } from 'react'
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { Button, Form, Segment } from 'semantic-ui-react'
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from './../../../app/stores/store';
+import { v4 as uuid } from 'uuid';
 
 const ActivityForm = () => {
+    const history = useHistory();
     const { activityStore } = useStore();
-    const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
+    const { createActivity, updateActivity, loading, loadingInitial, loadActivity } = activityStore;
+    const { id } = useParams<{ id: string }>();
 
-    const initialState = selectedActivity ?? {
+    const [activity, setActivity] = useState({
         id: '',
         title: '',
         date: '',
@@ -15,17 +20,31 @@ const ActivityForm = () => {
         city: '',
         venue: '',
         category: ''
-    };
+    });
+    useEffect(() => {
+        if (id) loadActivity(id).then(activity => setActivity(activity!))
 
-    const [activity, setActivity] = useState(initialState);
+    }, [id, loadActivity])
+
     function handleSumbit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
+        } else {
+            updateActivity(activity).then(() => history.push(`/activities/${activity.id}`))
+        }
+
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const { name, value } = event.target;
         setActivity({ ...activity, [name]: value })
     }
+
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
@@ -37,7 +56,7 @@ const ActivityForm = () => {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
+                <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
