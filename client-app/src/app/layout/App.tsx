105afc9761134b2b9797622dbde42918d645cf01
face2ram <bgsanap@gmail.com>
2021-09-17 09:38:25 +0530

    End of Section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,23 @@
 import { Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import NavBar from './NavBar';
 import HomePage from './../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from './../../features/activities/details/ActivityDetails';
+import NotFound from '../../features/errors/NotFound';
+import TestErrors from './../../features/errors/TestError';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const loacation = useLocation();
 
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -20,9 +25,14 @@ function App() {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route key={loacation.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route key={loacation.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+                <Route path='/errors' component={TestErrors} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
