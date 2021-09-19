ea541065e1ef9a6bb35ff9aee0417af46485b3cd
face2ram <bgsanap@gmail.com>
2021-09-19 18:07:50 +0530

    End of Section 11

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,9 +1,9 @@
 export interface Activity {
-    id: string
-    title: string
-    date: string
-    description: string
-    city: string
-    venue: string
+    id: string;
+    title: string;
+    date: Date | null;
+    description: string;
+    city: string;
+    venue: string;
     category: string
 }
